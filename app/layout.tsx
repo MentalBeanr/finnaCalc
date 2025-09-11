9c452aced28fff534f653a9fa694b5378c4650ed
Felipe Miranda <fm0291601@gmail.com>
2025-09-10 19:43:47 -0500

    fixed syntax error

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import { type Metadata } from 'next'
 import { Analytics } from "@vercel/analytics/next"
 import { ClerkProvider } from '@clerk/nextjs'
 import { Geist, Geist_Mono } from 'next/font/google'
-import './globals.css'git
+import './globals.css'
 
 const geistSans = Geist({
     variable: '--font-geist-sans',
