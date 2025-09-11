e651dbce734dd9bd3e1efeaac1974ebfcac7331e
Felipe Miranda <fm0291601@gmail.com>
2025-09-10 19:55:10 -0500

    imported stats

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { type Metadata } from 'next'
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { ClerkProvider } from '@clerk/nextjs'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
